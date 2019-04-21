de6d00a674d7adc3f4b682d541bae0b16508b121
James Hayward <james.hayward6@icloud.com>
2019-04-21 21:08:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,9 +7,8 @@ def square_array(array)
 #   end
 #   return array
 # end
-
-array.each do |numbers|
   i = 0
+array.each do |numbers|
   array[i] *= array[i]
   i += 1 #has to come after the operation because otherwise we'll miss the first array element
 end
