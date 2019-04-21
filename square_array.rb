d3370dc0d64089fbf4e34c6a30c7eeafeedeb7fe
James Hayward <james.hayward6@icloud.com>
2019-04-21 21:11:18 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  # your code here, without .each:
+#   i = 0 #needs to be before the loop command to work
+#   while i < array.length
+#     array[i] *= array[i]
+#     i += 1 #has to come after the operation because otherwise we'll miss the first array element
+#   end
+#   return array
+# end
+  i = 0 #needs to be before the loop command to work
+  array.each do |numbers|
+  array[i] *= array[i]
+  i += 1 #has to come after the operation because otherwise we'll miss the first array element
+end
 end
